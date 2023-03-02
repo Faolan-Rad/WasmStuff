8c686af48b7ffbbefd1e0699a91a7e8e9c2c1ab6
Faolan <46481567+Faolan-Rad@users.noreply.github.com>
2023-03-02 08:16:20 -0500

    Tests

---
+++ WasmTests/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using WebAssembly;
+using WebAssembly.Instructions;
+using WebAssembly.Runtime;
+
+namespace WasmTests
+{
+    public unsafe abstract class Sample
+    {
+        public abstract int Main(char* a);
+    }
+    internal unsafe class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var module = new Module();
+            var wasmCode = File.OpenRead("./Test.wasm");
+            using var instance = Compile.FromBinary<Sample>(wasmCode)(new ImportDictionary());
+            //try
+            //{
+                Console.WriteLine();
+            //}
+            //catch
+            //{
+            //    Console.WriteLine("Error");
+            //}
+
+        }
+    }
+}
